6b8fb5fcad0cb6857f7272a58502f83a82c7a12c
unknown <samuel.ajao@gmail.com>
2024-07-17 11:38:03 -0700

    Adding maven project files.

---
+++ src/main/java/car/dealership/CarDealershipApplication.java
@@ -0,0 +1,21 @@
+/*
+// Program: Promineo Tech Backend Java Back End Development Course
+// Author:  Samuel Ajao
+// Subject:  Spring Boot API FINAL PROJECT - Car Dealership
+// Create Date: July 17, 2024
+//
+*/
+package car.dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+
+	}
+
+}
